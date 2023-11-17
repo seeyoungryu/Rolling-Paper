446163ec13e458c3e5ec5aff7b7e2e874b91b51e
ayoung-jeon <wjsdkdud71@naver.com>
2023-11-17 17:01:07 +0900

    first commit

---
+++ src/main/java/com/sparta/rollingpaper/RollingpaperApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.rollingpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingpaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RollingpaperApplication.class, args);
+    }
+
+}
