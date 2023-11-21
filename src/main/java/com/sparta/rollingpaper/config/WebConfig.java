91f8e4f35d5457119ab01947ec86089dc1d021fe
ayoung-jeon <wjsdkdud71@naver.com>
2023-11-21 18:53:39 +0900

    CORS http로설정 변경

--- src/main/java/com/sparta/rollingpaper/config/WebConfig.java
+++ src/main/java/com/sparta/rollingpaper/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
