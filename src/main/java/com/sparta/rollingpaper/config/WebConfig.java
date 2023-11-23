60467707c79b48757029e76c1dc84b36accba039
seeyoungryu <140057709+seeyoungryu@users.noreply.github.com>
2023-11-23 18:46:22 +0900

    Update WebConfig.java
    alllowed origin 수정 > *

--- src/main/java/com/sparta/rollingpaper/config/WebConfig.java
+++ src/main/java/com/sparta/rollingpaper/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // OPTIONS 메서드 추가
                 .allowCredentials(true);
     }
